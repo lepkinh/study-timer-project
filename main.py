9917b8e386518c8b8bf4c5363f273f973340e5e3
NicolasLepki <nicolaslepki@gmail.com>
2024-09-22 13:59:52 -0400

    deleted all work and remade file directory, want simpler project

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 # main python file
+# TODO: implement GUI functionality, expand analytics class so algorithm actually does something, add error handling for edge cases, maybe do unit tests if care enough
+# NOTE: remove GUI, consider making this console based becasue GUI probably overcompicates things
 # author: Nicolas Lepki
 
